455a6e884d134e6cb6d9eaaf85782d0094b1dde9
Antônio César <antoniocesar16794@gmail.com>
2023-02-21 01:51:25 -0300

    modified:   extractor.py

--- extractor.py
+++ extractor.py
@@ -2,9 +2,11 @@ import binascii
 import cv2
 import os,argparse, io
 import pytesseract
+import math
 from PIL import Image
 import numpy as np
 
+RGBS = [{"RGB": (41, 38, 155), "game_name": "quina"}]
 
 def get_text_images_path(path):
     """
@@ -17,7 +19,7 @@ def get_text_images_path(path):
         
 
 def get_text_frame_cv2(frame):
-        image_text = pytesseract.image_to_string(frame, config='--oem 1 --psm 7 --dpi 70')
+        image_text = pytesseract.image_to_string(frame, config=r'--oem 1 --psm 7 --dpi 70')
         return image_text
 
 def split_image(img):
@@ -32,6 +34,27 @@ def get_rgb(image_data):
     rgb = im.convert('RGB') # get three R G B values
     RGB = rgb.getpixel((1, 1))
     return RGB
+
+def get_game_type(rgb1):
+    """
+    :type rgb: list 
+    """
+    for game in RGBS:
+        game_name = game["game_name"]
+        rgb2 = game["RGB"]
+        if(euclidean_distance(rgb1, rgb2) < 5.0):
+            return game_name
+        
+    return False
+
+    
+def euclidean_distance(color1, color2):
+    """
+    Distância euclidiana para calcular a distancia entre RGB1 e RGB2.
+    """
+    r1, g1, b1 = color1
+    r2, g2, b2 = color2
+    return math.sqrt((r2 - r1) ** 2 + (g2 - g1) ** 2 + (b2 - b1) ** 2)
 
 
 #type = get_type("result.jpg")
